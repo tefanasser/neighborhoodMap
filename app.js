4dfd3c43ed96a92b625b77c033a25a39394912ff
latifa almulhem <latifa.almulhem@gmail.com>
2018-02-20 16:05:39 +0300

    function ok

--- app.js
+++ app.js
@@ -1,60 +1,86 @@
 var list=[
-        {name:"Princess Norah University",
+        {index:0,
+            name:"Princess Norah University",
      lat:24.8464662,
     lng:46.7225421,
-    show:true},
-    {name:"Immam University",
+    show:ko.observable(true)},
+    {index:1,
+        name:"Immam University",
     lat:24.8159457,
     lng:46.6871576,
-    show:true},
-   {name:"King Saud University",
+    show:ko.observable(true)},
+   {index:2,
+       name:"King Saud University",
    lat:24.7162459,
     lng:46.6169191,
-    show:true
+    show:ko.observable(true)
    },
-    {name:"Prince Sultan University",
+    {index:3,
+        name:"Prince Sultan University",
     lat:24.7347468,
     lng:46.6953868,
-     show:true
+     show:ko.observable(true)
    },
-   {name:"Al-Faisal University",lat:24.6643064, lng:46.6737777,show:true}
+   {index:4,
+       name:"Al-Faisal University",lat:24.6643064, lng:46.6737777,show:ko.observable(true)}
    
 ];
 
-var viewModel= {
-    
-uniList: ko.observableArray(list),
-query:ko.observable(''),
-search:function(value) {
-    for (var i in list){
-    viewModel.uniList()[i].show=ko.observable(false);
-    }
+   var map;
+var markers=[];
+var bounds;
 
+var viewModel= {
+query:ko.observable(''),
+clicked:function(value){
+   markerclick(markers[value.index]);
+},
+filterList:ko.observableArray([]),
+search:function(value) {
+    
+    for (var x in list){      
+    viewModel.filterList()[x].show(false);
+    markers[x].visible=false;
+    markers[x].setMap(null);
+}
+    
     if (value == ''){
         for (var i in list){
-    list[i].show=true;
+     viewModel.filterList()[i].show(true);
+        markers[i].visible=true;
+        markers[i].setMap(map);
+
     }
-        return;
+         return;
     }
       for (var i in list) {
       if (list[i].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-        list[i].show=true;
+          
+        viewModel.filterList()[i].show(true);
+         markers[i].visible=true; 
+        markers[i].setMap(map);
+
       }
     }
   },
-}
+};
 
+for (var i in list){
+viewModel.filterList.push(list[i]);
+console.log(viewModel.filterList()[i]);
+}
 viewModel.query.subscribe(viewModel.search);
 ko.applyBindings(viewModel);
 
 
-var markers=[];
+
 
  function initMap() {
-            var map = new google.maps.Map(document.getElementById('map'), {
+            map = new google.maps.Map(document.getElementById('map'), {
                 zoom: 12,
                 center: {lat: 24.7241504, lng: 46.7152476}
         });
+     bounds = new google.maps.LatLngBounds();
         for (var i in list){
 var marker= new google.maps.Marker({
    position:{lat:list[i].lat,
@@ -63,6 +89,8 @@ var marker= new google.maps.Marker({
     title:list[i].name,
     id:i
 });
+bounds.extend(marker.position);
+map.fitBounds(bounds);
 markers.push(marker);
 marker.addListener('click',function(){
     markerclick(this);
@@ -78,7 +106,7 @@ function markerclick(marker){
    
 }
       function toggleBounce(marker) {
-        if (marker.getAnimation() !== null) {
+        if (marker.getAnimation()) {
           marker.setAnimation(null);
         } else {
           marker.setAnimation(google.maps.Animation.BOUNCE);
@@ -86,7 +114,8 @@ function markerclick(marker){
       }
         
  function populateInfowindow(marker,infoWindow) {
- toggleBounce(marker);
+     toggleBounce(marker);
+     //marker.setAnimation(google.maps.Animation.BOUNCE);
      var searchTerm=marker.title;
      var url= "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search="+searchTerm+"&limit=1&namespace=0&format=json";
      var info;
@@ -101,7 +130,8 @@ function markerclick(marker){
          infoWindow.setContent('<div><a href='+info+'>'+searchTerm+'</a></div>');
          infoWindow.open(map,marker);
          infoWindow.addListener('closeclick',function(){
-             this.setMarker(null);
+             //marker.setMarker(null);
+             infoWindow.close();
          });
      
     
